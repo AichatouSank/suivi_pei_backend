c1c7e8e512c402cc814291fd8a6e35ed586aab5b
Aichatou sankara <aichatousankara97@gmail.com>
2023-09-23 15:24:02 +0000

    ajout de regionController

---
+++ src/main/java/com/dgsi/suivievaluationpei/controller/RegionController.java
@@ -0,0 +1,61 @@
+package com.dgsi.suivievaluationpei.controller;
+
+import com.dgsi.suivievaluationpei.model.Region;
+import com.dgsi.suivievaluationpei.response.CustomResponse;
+import com.dgsi.suivievaluationpei.service.RegionService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/regions")
+@CrossOrigin
+public class RegionController {
+    private final RegionService regionService;
+    @PostMapping
+    public ResponseEntity<CustomResponse> AddRegion(@RequestBody @Valid Region region){
+        return ResponseEntity.ok(
+                CustomResponse.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("regions", regionService.addRegion(region)))
+                        .message("enregistrement de la region : "+ region.getNomRegion())
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+    @GetMapping
+    public ResponseEntity<CustomResponse> getAllRegions(){
+        return ResponseEntity.ok(
+                CustomResponse.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("liste des régions", regionService.findAllRegions()))
+                        .message("liste de toutes les régions")
+                        .statusCode(OK.value())
+                        .status(OK)
+                        .build()
+        );
+    }
+    @PutMapping
+    public ResponseEntity<CustomResponse> updateRegion(Region region){
+        return ResponseEntity.ok(
+                CustomResponse.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("region", regionService.updateRegion(region)))
+                        .message("mise à jour de la région" + region.getNomRegion())
+                        .statusCode(OK.value())
+                        .status(OK)
+                        .build()
+        );
+    }
+
+}

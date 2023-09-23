13d9cb7f9ea17a6c44b2d00aad14de30b32bd0c0
Aichatou sankara <aichatousankara97@gmail.com>
2023-09-23 15:23:05 +0000

    modification de regionService et ajout de regionController

--- src/main/java/com/dgsi/suivievaluationpei/service/RegionService.java
+++ src/main/java/com/dgsi/suivievaluationpei/service/RegionService.java
@@ -1,4 +1,12 @@
 package com.dgsi.suivievaluationpei.service;
 
+import com.dgsi.suivievaluationpei.model.Region;
+
+import java.util.List;
+
 public interface RegionService {
+    Region addRegion(Region region);
+    List<Region> findAllRegions();
+
+    Object updateRegion(Region region);
 }
--- src/main/java/com/dgsi/suivievaluationpei/service/implementation/RegionServiceImpl.java
+++ src/main/java/com/dgsi/suivievaluationpei/service/implementation/RegionServiceImpl.java
@@ -1,4 +1,31 @@
 package com.dgsi.suivievaluationpei.service.implementation;
 
-public class RegionServiceImpl {
+import com.dgsi.suivievaluationpei.model.Region;
+import com.dgsi.suivievaluationpei.repository.RegionRepository;
+import com.dgsi.suivievaluationpei.service.RegionService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class RegionServiceImpl implements RegionService {
+    private final RegionRepository regionRepository;
+
+    @Override
+    public Region addRegion(Region region) {
+        return regionRepository.save(region);
+    }
+
+    @Override
+    public List<Region> findAllRegions() {
+        return regionRepository.findAll();
+    }
+
+    @Override
+    public Object updateRegion(Region region) {
+        return regionRepository.save(region);
+    }
 }
